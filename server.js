0621406854a9570607a5ff4f02218b2ce7812e86
pabloalexisudea <ealexis.duque@udea.edu.co>
2019-08-19 17:50:24 -0500

    cors

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ app.use(bodyParserURLEncoded);
 
 //app.use(cors());
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', 'https://front-petrolitos.herokuapp.com/auth/register');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  next();
+});
+
 app.use('/api', router);
 
 authRoutes(router);
