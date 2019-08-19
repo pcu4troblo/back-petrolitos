aa60fd8596b808372a6c295d9d003b0e393be424
pabloalexisudea <ealexis.duque@udea.edu.co>
2019-08-19 17:57:14 -0500

    cors

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(bodyParserURLEncoded);
 //app.use(cors());
 
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.Header('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.Header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.Header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   next();
 });
