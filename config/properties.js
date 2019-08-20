de931f711ea184acc1ede282d19fa16660ad7272
pabloalexisudea <ealexis.duque@udea.edu.co>
2019-08-19 20:32:35 -0500

    first commit

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  DB: 'mongodb://localhost:27017/loginMean'
+  DB: 'mongodb+srv://juanpablor56:PwC6e0ufVCqIWsaI@petrolitosdb-9zidt.mongodb.net/test?retryWrites=true&w=majority'
 }
